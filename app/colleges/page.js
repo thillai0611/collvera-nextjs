0f41ef6804ac15c7c87414c3c0f019d977f72f0d
thillai0611 <81110894+thillai0611@users.noreply.github.com>
2026-04-09 18:17:06 +0530

    Add files via upload

--- app/colleges/page.js
+++ app/colleges/page.js
@@ -1,5 +1,6 @@
 import { getColleges } from '../../lib/supabase'
-import CollegesClient from './CollegesClient'
+import { COLLEGES }    from '../../lib/colleges'
+import CollegesClient  from './CollegesClient'
 
 export const revalidate = 0
 
@@ -9,6 +10,21 @@ export const metadata = {
 }
 
 export default async function CollegesPage() {
-  const colleges = await getColleges()
+  const raw = await getColleges()
+
+  const colleges = raw.map(c => {
+    const local = COLLEGES[c.slug] || {}
+    return {
+      ...c,
+      avg_package:        local.avgPkg    ? local.avgPkg                 : null,
+      cat_cutoff:         local.exams?.CAT                               || null,
+      tags:               local.tags                                     || [],
+      verdict:            local.verdict                                  || null,
+      work_exp_required:  local.workExpRequired                          || false,
+      work_exp_preferred: local.workExpPreferred                         || false,
+      top_recruiters:     local.topRecruiters                            || [],
+    }
+  })
+
   return <CollegesClient initialColleges={colleges} />
 }
